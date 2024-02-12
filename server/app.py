d89a981a9d8e33591875aaf0b7804118813a5f4e
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-12 14:07:52 +0300

    Setup consumer_key and consumer_secret

--- server/app.py
+++ server/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
 
 
 app = Flask(__name__)
 api = Api(app)
+
+# mpesa details
+consumer_key = environ.get("CONSUMER_KEY")
+consumer_secret = environ.get("CONSUMER_SECRET")
 
 
 class Index(Resource):
