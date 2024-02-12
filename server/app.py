cb13fbc6b027e52a5b6384a1318d73d03753d7eb
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-12 14:22:54 +0300

    Implement AccessToken resource == for generating access_token

--- server/app.py
+++ server/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api, Resource
 from dotenv import load_dotenv
 from os import environ
+import requests
+from requests.auth import HTTPBasicAuth
 
 load_dotenv()
 
@@ -18,8 +20,26 @@ class Index(Resource):
     def get(self):
         return "Hello World", 200
 
+# function to get access_token
+
+
+def access_token():
+    mpesa_auth_url = environ.get("MPESA_AUTH_URL")
+    data = requests.get(mpesa_auth_url, auth=HTTPBasicAuth(
+        consumer_key, consumer_secret)).json()
+
+    return data
+
+
+class AccessToken(Resource):
+    def get(self):
+        data = access_token()
+
+        return data["access_token"]
+
 
 api.add_resource(Index, "/")
+api.add_resource(AccessToken, "/access_token")
 
 
 if __name__ == "__main__":
