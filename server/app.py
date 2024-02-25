df534d20ee5017aabd32e9d6398847859bddec47
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-25 23:21:59 +0300

    Set up basic flask application

--- server/app.py
+++ server/app.py
@@ -1,46 +1,11 @@
 from flask import Flask
-from flask_restful import Api, Resource
-from dotenv import load_dotenv
-from os import environ
-import requests
-from requests.auth import HTTPBasicAuth
-
-load_dotenv()
+from controllers.mpesa_controllers import mpesa_bp
 
 
 app = Flask(__name__)
-api = Api(app)
 
-# mpesa details
-consumer_key = environ.get("CONSUMER_KEY")
-consumer_secret = environ.get("CONSUMER_SECRET")
-
-
-class Index(Resource):
-    def get(self):
-        return "Hello World", 200
-
-# function to get access_token
-
-
-def access_token():
-    mpesa_auth_url = environ.get("MPESA_AUTH_URL")
-    data = requests.get(mpesa_auth_url, auth=HTTPBasicAuth(
-        consumer_key, consumer_secret)).json()
-
-    return data
-
-
-class AccessToken(Resource):
-    def get(self):
-        data = access_token()
-
-        return data["access_token"]
-
-
-api.add_resource(Index, "/")
-api.add_resource(AccessToken, "/access_token")
+app.register_blueprint(mpesa_bp)
 
 
 if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
